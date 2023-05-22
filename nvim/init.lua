074de8ca26d3117c20dc661a675f70cc8400cdec
Sy Tran <sytrancsvn@gmail.com>
2023-05-22 22:55:47 +0700

    update buffline

--- nvim/init.lua
+++ nvim/init.lua
@@ -425,7 +425,6 @@ local servers = {
   pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -3,10 +3,27 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'Buffer: ' .. desc
+      end
+
+      vim.keymap.set('n', keys, func, { desc = desc })
+    end
+
+
     require('bufferline').setup {}
-    vim.keymap.set('n', '<C-c>', function()
-      require('bufferline.commands').cycle(1)
-      vim.cmd("bd #")
-    end, { desc = "Close current buffer" })
+    nmap('<C-c>', function()
+        if require('bufferline.utils').get_buf_count() > 1 then
+          require('bufferline.commands').cycle(1)
+          vim.cmd("bd #")
+        else
+          vim.cmd("bd")
+        end
+      end,
+      "Close current buffer"
+    )
+    nmap('<C-n>', function() require('bufferline').cycle(1) end, "Next buffer")
+    nmap('<C-p>', function() require('bufferline').cycle(-1) end, "Previous buffer")
   end
 }
