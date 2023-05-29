81a2ee7a10a3cf396b8a34962bfb65c77d1af94a
Sy Tran Dung <sytrancsvn@gmail.com>
2023-05-29 21:25:44 +0700

    emoji plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,7 +94,12 @@ require('lazy').setup({
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-emoji'
+    },
   },
 
   -- Useful plugin to show you pending keybinds.
@@ -504,6 +509,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'emoji' },
   },
 }
 
