2b9fc0fc43be1062015138fc13aa4a7a887944a1
Sy Tran <13009812+sytranvn@users.noreply.github.com>
2023-10-20 20:46:57 +0700

    many

--- nvim/init.lua
+++ nvim/init.lua
@@ -255,7 +255,17 @@ vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fa', require('telescope.builtin').live_grep, { desc = '[F]ind [A]ll' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fe', function()
+  require('telescope.builtin').symbols { sources = { 'emoji', 'gitmoji' } }
+end, { desc = '[F]ind [E]moji' })
 
+vim.keymap.set('n', '<leader>fm', function()
+  require('telescope.builtin').symbols { sources = { 'math', 'gitmoji' } }
+end, { desc = '[F]ind [M]ath' })
+
+vim.keymap.set('n', '<leader>fi', function()
+  require('telescope.builtin').symbols { sources = { 'emoji', 'gitmoji', 'julia' } }
+end, { desc = '[F]ind [I]con' })
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
