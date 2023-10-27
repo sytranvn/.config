e381af60157370b290f972812ea4c73a684888e7
Sy Tran Dung <13009812+sytranvn@users.noreply.github.com>
2023-10-27 11:31:52 +0700

    moving keys update to alt

---
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -0,0 +1,21 @@
+-- require "lspconfig".efm.setup {
+--   init_options = { documentFormatting = true },
+--   settings = {
+--     rootMarkers = { ".git/" },
+--     languages = {
+--       lua = {
+--         { formatCommand = "lua-format -i", formatStdin = true }
+--       },
+--       javascript = {
+--         {
+--           formatCommand = "prettierd", formatStdin = true
+--         }
+--       },
+--       python = {
+--         {
+--           formatCommand = "python-isort", formatStdin = true
+--         }
+--       }
+--     }
+--   }
+-- }
