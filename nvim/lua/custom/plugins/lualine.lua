23b6cc7141c110c4331fd6c6f98151d2a20ee3fe
Sy Tran Dung <13009812+sytranvn@users.noreply.github.com>
2023-11-29 14:43:05 +0700

    Buffer map

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -24,19 +24,35 @@ return {
         }
       },
       winbar = {
-        lualine_c = { 'filename' },
+        -- lualine_c = { 'filename' },
       },
       inactive_winbar = {
         lualine_c = { 'filename' },
       },
       tabline = {
-        lualine_a = {},
+        lualine_a = { 'filename' },
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = { 'tabs' }
+        lualine_z = {}
       }
     })
+    vim.api.nvim_create_user_command("BuffCloseOthers", function()
+      local bufs = vim.api.nvim_list_bufs()
+      local current_buf = vim.api.nvim_get_current_buf()
+      for _, i in ipairs(bufs) do
+        if i ~= current_buf then
+          if vim.api.nvim_buf_get_option(i, "modified") then
+            vim.api.nvim_echo("Unsaved buffer " .. i, false)
+          else
+            vim.api.nvim_buf_delete(i, {})
+          end
+        end
+      end
+    end, {})
+
+    vim.keymap.set('n', 'gn', ':bn<CR>', { silent = true })
+    vim.keymap.set('n', 'gN', ':bp<CR>', { silent = true })
   end
 }
