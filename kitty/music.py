ee97fb77ba83a5afd751231d291595d22ce3e6d7
Sy Tran Dung <sytrancsvn@gmail.com>
2024-04-23 14:13:39 +0700

    fix time

--- kitty/music.py
+++ kitty/music.py
@@ -6,11 +6,9 @@ from datetime import datetime
 dir = os.path.dirname(__file__)
 def get_song():
     if platform.system() == 'Darwin':
-        if os.path.exists(f"{dir}/.spotify"):
-            stat = os.stat(f"{dir}/.spotify")
-            if datetime.now().timestamp() - stat.st_mtime > 10000:
-                with open(f"{dir}/.spotify", "r") as f:
-                    return f.read().rstrip()
+        if not os.path.exists(f"{dir}/.spotify") or datetime.now().timestamp() - os.stat(f"{dir}/.spotify").st_mtime < 10:
+            with open(f"{dir}/.spotify", "r") as f:
+                return f.read().rstrip()
         else:
             song = subprocess.check_output(f"osascript {dir}/osx_status.script".split()).decode()
             with open(f"{dir}/.spotify", "w") as f:
