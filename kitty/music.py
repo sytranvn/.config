fc841c4f5e4a39026ac48def5b29271816438145
Sy Tran Dung <sytrancsvn@gmail.com>
2024-04-27 14:40:13 +0700

    check proper pid

--- kitty/music.py
+++ kitty/music.py
@@ -2,8 +2,11 @@ import subprocess
 import platform
 import os
 from datetime import datetime
+
+from psutil import NoSuchProcess
 dir = os.path.dirname(__file__)
 spotify_path = f"{dir}/.spotify"
+
 
 def get_song():
     if platform.system() == 'Darwin':
@@ -11,7 +14,8 @@ def get_song():
             with open(spotify_path, "r") as f:
                 return f.read().rstrip()
         else:
-            song = subprocess.check_output(f"osascript {dir}/osx_status.script".split()).decode()
+            song = subprocess.check_output(
+                f"osascript {dir}/osx_status.script".split()).decode()
             with open(spotify_path, "w") as f:
                 f.write(song)
 
@@ -20,28 +24,33 @@ def get_song():
         return get_ubuntu_song()
 
 
-
 def get_ubuntu_song():
     import psutil
     import os
 
     dbuspid_path = f"{dir}/.dbpid"
+    cmd = ["python3", f"{dir}/music.py"]
     if os.path.exists(dbuspid_path):
         with open(dbuspid_path, "r") as f:
-            if psutil.pid_exists(int(f.read())):
-                if os.path.exists(spotify_path):
-                    song = open(spotify_path, "r").read()
-                    return song
+            try:
+                p = psutil.Process(int(f.read()))
+                if p.cmdline() == cmd:
+                    if os.path.exists(spotify_path):
+                        song = open(spotify_path, "r").read()
+                        return song
+            except psutil.NoSuchProcess:
+                pass
 
-    p = subprocess.Popen(["python3", f"{dir}/music.py"], start_new_session=True)
+    p = subprocess.Popen(
+        cmd, start_new_session=True)
     with open(dbuspid_path, "w") as f:
         f.write(str(p.pid))
-            
+
 
 def start_dbus_monitor():
     import dbus
     from dbus.mainloop.glib import DBusGMainLoop
-    from gi.repository import GLib
+    from gi.repository import GLib  # noqua
 
     def notifications(bus, message):
         args = message.get_args_list()
@@ -52,27 +61,37 @@ def start_dbus_monitor():
     DBusGMainLoop(set_as_default=True)
 
     bus = dbus.SessionBus()
-    bus.add_match_string_non_blocking("eavesdrop=true, interface='org.freedesktop.Notifications', member='Notify'")
+    bus.add_match_string_non_blocking(
+        "eavesdrop=true, interface='org.freedesktop.Notifications', "
+        "member='Notify'")
     bus.add_message_filter(notifications)
 
     mainloop = GLib.MainLoop()
     mainloop.run()
 
+
 def get_playing_song():
     song = ""
     try:
-        meta = subprocess.check_output("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata".split())
+        meta = subprocess.check_output(
+            "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
+            "/org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get "
+            "string:org.mpris.MediaPlayer2.Player string:Metadata".split())
         lines = meta.splitlines()
-        entry_line = next((lines[i+1] for i in range(len(lines )) if b"xesam:title" in lines[i]), None)
+        entry_line = next(
+            (lines[i+1] for i in range(len(lines))
+             if b"xesam:title" in lines[i]),
+            None
+        )
         if entry_line:
             import re
             song = re.search('"(.*)"', entry_line.decode()).group(1)
-    except:
+    except Exception:
         ...
     with open(spotify_path, "w") as f:
         f.write(song)
 
+
 if __name__ == "__main__":
     get_playing_song()
     start_dbus_monitor()
-       
